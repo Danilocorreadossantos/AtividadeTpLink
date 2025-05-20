b7b0ccd13400f52e078119b753d53961569462e1
Danilo Correa <danilocorrea.santos@hotmail.com>
2025-05-20 19:45:11 -0300

    Bug Corrigido

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,13 @@ var  usuario = document.getElementById("button-submit")
 
 function login() {
 
+    
+
     var campoUsuario = document.getElementById("usuario").value
     var campoSenha = document.getElementById("senha").value
     var campoCodigo = document.getElementById("codigo").value
 
-    if(campoUsuario == admin && campoSenha == 1234 && campoCodigo == tplink2025 ) {
+    if(campoUsuario == "admin" && campoSenha == "1234" && campoCodigo == "tplink2025" ) {
         window.location.replace("painel.html")
     }   else{
         alert("Ops! Você errou e-mail e senha")
@@ -14,5 +16,4 @@ function login() {
     }
 }   
 
-button-submit.addEventListener("click", login())
-}
+usuario.addEventListener("click", login)
